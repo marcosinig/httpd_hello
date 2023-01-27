fbe662c109a794f95d906c12ddd05c549107526e
Marco Sinigaglia <marco.sinigaglia@techdata.com>
2023-01-27 14:20:28 +0100

    initial commit

---
+++ httpd_hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo service httpd start
+sudo chkconfig httpd on
+cd /var/www/html
+echo"<html><h1>This is my Apache Web Server 01</h1></html>" > index.html

91f8a540577e2b8839a2bb85df2b9714d060d07a
Marco Sinigaglia <marco.sinigaglia@techdata.com>
2023-01-27 16:53:07 +0100

    fix script

--- httpd_hello_world.sh
+++ httpd_hello_world.sh
@@ -4,4 +4,4 @@ sudo yum install httpd -y
 sudo service httpd start
 sudo chkconfig httpd on
 cd /var/www/html
-echo"<html><h1>This is my Apache Web Server 01</h1></html>" > index.html
+echo "<html><h1>This is my Apache Web Server 01</h1></html>" > index.html
